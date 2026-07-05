bcd7bbe9ecc967ad4195e55209493d11724f8f46
PranaliPatil2727 <avantika1901@student.sfit.ac.in>
2026-07-05 16:32:33 +0530

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Team from "./pages/Team";
-import FAQ from "./pages/FAQ";
-import Contact from "./pages/Contact";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Team from "./Pages/Team";
+import FAQ from "./Pages/FAQ";
+import Contact from "./Pages/Contact";
 
 export default function App() {
   return (
